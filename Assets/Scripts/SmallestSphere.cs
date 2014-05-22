e2ddd4e8c8ba0b0ad5699f0473d40a88040c6d2a
FlorianBasso <florian@ideanov.com>
2014-05-22 09:59:04 +0200

    comment script

--- Assets/Scripts/SmallestSphere.cs
+++ Assets/Scripts/SmallestSphere.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class SmallestSphere : MonoBehaviour {
 	private float _radius;
 	private Vector3 _center;
-	//For welzl calculations
 	private const float RadiusEpsilon = 1.00001f;
 	private GameObject sphere;
 	
@@ -33,18 +32,20 @@ public class SmallestSphere : MonoBehaviour {
 	
 	void ComputeFromPrimitives(Vector3[] vertices, int[] indices)
 	{
+		//Create a copy of the mesh 
 		Vector3[] copy = new Vector3[indices.Length];
 		
 		for(int i = 0; i < indices.Length; i++) {
 			copy[i] = transform.TransformPoint(vertices[indices[i]]); // test
 			//copy[i] = vertices[indices[i]];
 		}
-		
+		//Call the Welzl's algorithm
 		CalculateWelzl(copy, copy.Length, 0, 0);
 	}
 	
 	//Welzl minimum bounding sphere algorithm
 	void CalculateWelzl(Vector3[] points, int length, int supportCount, int index) {
+		//Adapt the bounding Sphere (radius, center, ...)
 		switch(supportCount) {
 			case 0:
 				_radius = 0;
@@ -55,11 +56,8 @@ public class SmallestSphere : MonoBehaviour {
 				_center = points[index-1];
 				break;
 			case 2:
-				
 				SetSphere(points[index-1], points[index-2]);
-				
 				break;
-				
 			case 3:
 				SetSphere(points[index-1], points[index-2], points[index-3]);
 				break;
@@ -67,14 +65,15 @@ public class SmallestSphere : MonoBehaviour {
 				SetSphere(points[index-1], points[index-2], points[index-3], points[index-4]);
 				return;
 		}
-		
+		//For each point
 		for(int i = 0; i < length; i++)
 		{
+			//Get the current point's coordinate
 			Vector3 comp = points[i + index];
 			float distSqr;
-			
-			distSqr = (comp-_center).sqrMagnitude;
-			
+			//Returns the squared length of this vector : (comp - _center)
+			distSqr = (comp - _center).sqrMagnitude;
+			//Determine if comp is inside or outside of the bounding sphere	
 			if(distSqr - (_radius * _radius) > RadiusEpsilon - 1.0f) {
 				for(int j = i; j > 0; j--) {
 					Vector3 a = points[j + index];
@@ -82,12 +81,13 @@ public class SmallestSphere : MonoBehaviour {
 					points[j + index] = b;
 					points[j - 1 + index] = a;
 				}
+				//Increment supportCount and index then do a recursive call
 				CalculateWelzl(points, i, supportCount + 1, index + 1);
 			}
 		}
 	}
 	
-	//For Welzl calc - 2 support points
+	//2 support points for Welzl algorithm 
 	void SetSphere(Vector3 O, Vector3 A)
 	{
 		Radius = (float) System.Math.Sqrt(((A.x - O.x) * (A.x - O.x) + (A.y - O.y)
@@ -96,29 +96,32 @@ public class SmallestSphere : MonoBehaviour {
 		float y = (1 - .5f) * O.y + .5f * A.y;
 		float z = (1 - .5f) * O.z + .5f * A.z;
 		
-		// TODO:
-		SetCenter(x, y, z);
-		
+		SetCenter(x, y, z);		
 	}
 	
-	//For Welzl calc - 3 support points
-	void SetSphere(Vector3 O, Vector3 A, Vector3 B) {
+	//3 support points for Welzl algorithm 
+	void SetSphere(Vector3 O, Vector3 A, Vector3 B)
+	{
 		Vector3 a = A - O;
 		Vector3 b = B - O;
+		//Get cross product of a and b
 		Vector3 aCrossB = Vector3.Cross(a, b);
+		//Get dot/scalar product of aCrossB.aCrossB
 		float denom = 2.0f * Vector3.Dot(aCrossB, aCrossB);
-		if(denom == 0) {
+		if(denom == 0) 
+		{
 			_center = Vector3.zero;
 			_radius = 0;
-		} else {
-			
+		}
+		else 
+		{	
 			Vector3 o = ((Vector3.Cross(aCrossB, a) * b.sqrMagnitude)+ (Vector3.Cross(b, aCrossB) * a.sqrMagnitude)) / denom;
 			_radius = o.magnitude * RadiusEpsilon;
 			_center = O + o;
 		}
 	}
 	
-	//For Welzl calc - 4 support points
+	//4 support points for Welzl algorithm 	
 	void SetSphere(Vector3 O, Vector3 A, Vector3 B, Vector3 C) {
 		Vector3 a = A - O;
 		Vector3 b = B - O;
@@ -126,10 +129,13 @@ public class SmallestSphere : MonoBehaviour {
 		
 		float denom = 2.0f * (a.x * (b.y * c.z - c.y * b.z) - b.x
 		                      * (a.y * c.z - c.y * a.z) + c.x * (a.y * b.z - b.y * a.z));
-		if(denom == 0) {
+		if(denom == 0) 
+		{
 			_center = Vector3.zero;
 			_radius = 0;
-		} else {
+		} 
+		else 
+		{
 			Vector3 o = ((Vector3.Cross(a, b) * c.sqrMagnitude)
 			             + (Vector3.Cross(c, a) * b.sqrMagnitude)
 			             + (Vector3.Cross(b, c) * a.sqrMagnitude)) / denom;
@@ -140,7 +146,9 @@ public class SmallestSphere : MonoBehaviour {
 	//Set the center of the bounding sphere
 	void SetCenter(float x,float y,float z)
 	{
+		//Resize the bounding sphere
 		sphere.transform.localScale = new Vector3(_radius*2,_radius*2,_radius*2);
+		//Place the bounding sphere
 		sphere.transform.position = new Vector3(x,y,z);
 	}
 }
